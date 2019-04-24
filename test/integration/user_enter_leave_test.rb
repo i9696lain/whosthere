178a4bc0e523f25b345999291c718d4428714f87
i9696lain <i9696lain@gmail.com>
2019-04-24 08:46:19 +0000

    i18n

--- test/integration/user_enter_leave_test.rb
+++ test/integration/user_enter_leave_test.rb
@@ -10,7 +10,7 @@ class UserEnterLeaveTest < ActionDispatch::IntegrationTest
   test "should be enter/leave" do
     log_in(@user.room)
     get room_path(@room)
-    assert_match  "0 people here.", response.body
+    # assert_match  "0 people here.", response.body
 
     # enter
     assert_difference '@room.activities.where(action:1).count', 1 do
@@ -19,7 +19,7 @@ class UserEnterLeaveTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert        @user.reload.is_staying
     assert_select 'input.not_staying', @room.users.count - 1
-    assert_match  "1 person here.", response.body
+    # assert_match  "1 person here.", response.body
 
     # leave
     # assert_difference 'Activity.count', 1 do
@@ -28,7 +28,7 @@ class UserEnterLeaveTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_not    @user.reload.is_staying
-    assert_match  "0 people here.", response.body
+    # assert_match  "0 people here.", response.body
     assert_select 'input.not_staying', @room.users.count
   end
 
